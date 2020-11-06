257a5a6dad12d1354b66ba8b1c5b37fbdc7ba1cf
Ben <swerdlowbenjamin@gmail.com>
2020-11-06 01:54:02 -0800

    Add 'export/chemistry/' from commit '56a4b5489bf58e058c64a162319de5c398812fe5'
    git-subtree-dir: export/chemistry
    git-subtree-mainline: 56a4b5489bf58e058c64a162319de5c398812fe5
    git-subtree-split: 56a4b5489bf58e058c64a162319de5c398812fe5

---
+++ export/chemistry/src/routes/index.svelte
@@ -0,0 +1,49 @@
+<script>
+	import fuck from '../assets/fuck.gif';
+</script>
+
+<style>
+	h1, figure {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Fuck Chemistry</title>
+</svelte:head>
+
+<h1>Fuck Chemistry!</h1>
+
+<figure>
+	<img alt="fuck" src="{fuck}">
+	<figcaption>Hopefully this site helps u do good on your next chemistry thing</figcaption>
+</figure>
+
