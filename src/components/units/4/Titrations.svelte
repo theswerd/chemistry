c6046a9bcea4012607999e5fb5f8d1ee75d73a82
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2020-11-23 03:29:34 +0000

    quick fix

--- src/components/units/4/Titrations.svelte
+++ src/components/units/4/Titrations.svelte
@@ -1,6 +1,6 @@
 <script>
     import VolumeBasedOnMolarity from './titrations/volumeBasedOnMolarity.svelte';
-    import MolarMassBasedOnMolarity from './titrations/molarMassFromMassMolarityVolume.svelte';
+    import MolarMassBasedOnMolarity from './titrations/molarMassBasedOnMolarity.svelte';
 </script>
 <h2>Titrations</h2>
 <VolumeBasedOnMolarity/>
