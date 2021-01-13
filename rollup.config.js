6f0106bf21e08dceab27f7e625ecc0ef3c5a7a19
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2021-01-12 23:40:37 -0800

    Updates

--- rollup.config.js
+++ rollup.config.js
@@ -10,6 +10,8 @@ import config from 'sapper/config/rollup.js';
 import pkg from './package.json';
 import autoPreprocess from 'svelte-preprocess';
 import typescript from '@rollup/plugin-typescript';
+import json from 'rollup-plugin-json';
+
 //import typescript from "rollup-plugin-typescript2"; // add
 
 const mode = process.env.NODE_ENV;
@@ -17,108 +19,117 @@ const dev = mode === 'development';
 const legacy = !!process.env.SAPPER_LEGACY_BUILD;
 
 const onwarn = (warning, onwarn) =>
-	(warning.code === 'MISSING_EXPORT' && /'preload'/.test(warning.message)) ||
-	(warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) ||
-	onwarn(warning);
+    (warning.code === 'MISSING_EXPORT' && /'preload'/.test(warning.message)) ||
+    (warning.code === 'CIRCULAR_DEPENDENCY' && /[/\\]@sapper[/\\]/.test(warning.message)) ||
+    onwarn(warning);
 
 export default {
-	client: {
-		input: config.client.input().replace(/\.js$/, ".ts"), // edit here
-		output: config.client.output(),
-		plugins: [
-			replace({
-				'process.browser': true,
-				'process.env.NODE_ENV': JSON.stringify(mode)
-			}),
-			svelte({
-				dev,
-				hydratable: true,
-				emitCss: true,
-				preprocess: autoPreprocess(),
-			}),
-			typescript(),
-			url({
-				sourceDir: path.resolve(__dirname, 'src/node_modules/images'),
-				publicPath: '/client/'
-			}),
-			resolve({
-				browser: true,
-				dedupe: ['svelte']
-			}),
-			commonjs(),
+    client: {
+        input: config.client.input().replace(/\.js$/, ".ts"), // edit here
+        output: config.client.output(),
+        plugins: [
+            replace({
+                'process.browser': true,
+                'process.env.NODE_ENV': JSON.stringify(mode)
+            }),
+            svelte({
+                dev,
+                hydratable: true,
+                emitCss: true,
+                preprocess: autoPreprocess(),
+            }),
+            typescript(),
+            json({
+                // All JSON files will be parsed by default,
+                // but you can also specifically include/exclude files
+                include: 'src/**',
+                // for tree-shaking, properties will be declared as
+                // variables, using either `var` or `const`
+                preferConst: true, // Default: false
+                // ignores indent and generates the smallest code
+                compact: true,
+                namedExports: true // Default: true
+            }),
+            url({
+                sourceDir: path.resolve(__dirname, 'src/node_modules/images'),
+                publicPath: '/client/'
+            }),
+            resolve({
+                browser: true,
+                dedupe: ['svelte']
+            }),
+            commonjs(),
 
-			legacy && babel({
-				extensions: ['.js', '.mjs', '.html', '.svelte'],
-				babelHelpers: 'runtime',
-				exclude: ['node_modules/@babel/**'],
-				presets: [
-					['@babel/preset-env', {
-						targets: '> 0.25%, not dead'
-					}]
-				],
-				plugins: [
-					'@babel/plugin-syntax-dynamic-import',
-					['@babel/plugin-transform-runtime', {
-						useESModules: true
-					}]
-				]
-			}),
+            legacy && babel({
+                extensions: ['.js', '.mjs', '.html', '.svelte'],
+                babelHelpers: 'runtime',
+                exclude: ['node_modules/@babel/**'],
+                presets: [
+                    ['@babel/preset-env', {
+                        targets: '> 0.25%, not dead'
+                    }]
+                ],
+                plugins: [
+                    '@babel/plugin-syntax-dynamic-import', ['@babel/plugin-transform-runtime', {
+                        useESModules: true
+                    }]
+                ]
+            }),
 
-			!dev && terser({
-				module: true
-			})
-		],
+            !dev && terser({
+                module: true
+            })
+        ],
 
-		preserveEntrySignatures: false,
-		onwarn,
-	},
+        preserveEntrySignatures: false,
+        onwarn,
+    },
 
-	server: {
-		input: config.server.input().server.replace(/\.js$/, ".ts"),
-		output: config.server.output(),
-		plugins: [
-			replace({
-				'process.browser': false,
-				'process.env.NODE_ENV': JSON.stringify(mode)
-			}),
-			svelte({
-				generate: 'ssr',
-				hydratable: true,
-				preprocess: autoPreprocess(),
-				dev
-			}),
-			url({
-				sourceDir: path.resolve(__dirname, 'src/node_modules/images'),
-				publicPath: '/client/',
-				emitFiles: false // already emitted by client build
-			}),
-			resolve({
-				dedupe: ['svelte']
-			}),
-			commonjs(),
-			typescript(),
+    server: {
+        input: config.server.input().server.replace(/\.js$/, ".ts"),
+        output: config.server.output(),
+        plugins: [
+            replace({
+                'process.browser': false,
+                'process.env.NODE_ENV': JSON.stringify(mode)
+            }),
+            svelte({
+                generate: 'ssr',
+                hydratable: true,
+                preprocess: autoPreprocess(),
+                dev
+            }),
+            url({
+                sourceDir: path.resolve(__dirname, 'src/node_modules/images'),
+                publicPath: '/client/',
+                emitFiles: false // already emitted by client build
+            }),
+            resolve({
+                dedupe: ['svelte']
+            }),
+            commonjs(),
+            typescript(),
 
-		],
-		external: Object.keys(pkg.dependencies).concat(require('module').builtinModules),
+        ],
+        external: Object.keys(pkg.dependencies).concat(require('module').builtinModules),
 
-		preserveEntrySignatures: 'strict',
-		onwarn,
-	},
+        preserveEntrySignatures: 'strict',
+        onwarn,
+    },
 
-	serviceworker: {
-		input: config.serviceworker.input(),
-		output: config.serviceworker.output(),
-		plugins: [
-			resolve(),
-			replace({
-				'process.browser': true,
-				'process.env.NODE_ENV': JSON.stringify(mode)
-			}),
-			commonjs(),
-			!dev && terser()
-		],
+    serviceworker: {
+        input: config.serviceworker.input(),
+        output: config.serviceworker.output(),
+        plugins: [
+            resolve(),
+            replace({
+                'process.browser': true,
+                'process.env.NODE_ENV': JSON.stringify(mode)
+            }),
+            commonjs(), !dev && terser()
+        ],
 
-		preserveEntrySignatures: false,
-		onwarn,
-	}
+        preserveEntrySignatures: false,
+        onwarn,
+    }
 };
--- src/routes/units/6.svelte
+++ src/routes/units/6.svelte
@@ -2,6 +2,7 @@
 import HHreq from "../../components/units/6/heat_mass_fussion/h_hreq.svelte";
 import HMass from "../../components/units/6/heat_mass_fussion/h_mass.svelte";
 import HreqMass from "../../components/units/6/heat_mass_fussion/hreq_mass.svelte";
+import ThermodynamicData from "../../components/units/6/thermodynamic_data.svelte";
 
 </script>
 <svelte:head>
@@ -11,7 +12,8 @@ import HreqMass from "../../components/units/6/heat_mass_fussion/hreq_mass.svelt
 <h1>Unit 6: Thermochemistry</h1>
 <p style="text-align:center">Note: This page is currently under development, there are no guarantees that answers are correct</p>
 <div style="height:15px" />
-<h1>Heat - Mass - Heat<sub>req</sub></h1>
+<h2>Heat - Mass - Heat<sub>req</sub></h2>
 <HHreq/>
 <HMass/>
 <HreqMass/>
+<ThermodynamicData/>
