5256d4069ac4cc8137ccc62d9bc9d735196db55c
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2021-01-13 01:13:49 -0800

    small things

--- src/components/units/6/heat_mass_fussion/q_mcat.svelte
+++ src/components/units/6/heat_mass_fussion/q_mcat.svelte
@@ -14,40 +14,100 @@
     }
 
     $: {
-        if (mass && heat_capacity && temp_change != null) {
+        if (
+            heat != null &&
+            mass != null &&
+            heat_capacity != null &&
+            temp_change != null
+        ) {
+            //FILLER
+        } else if (
+            mass != null &&
+            heat_capacity != null &&
+            temp_change != null
+        ) {
             heat = mass * heat_capacity * temp_change;
             make_bold = 1;
-        } else if (heat && heat_capacity && temp_change != null) {
+        } else if (
+            heat != null &&
+            heat_capacity != null &&
+            temp_change != null
+        ) {
             mass = heat / (heat_capacity * temp_change);
             make_bold = 2;
-        } else if (heat && mass && temp_change) {
+        } else if (heat != null && mass != null && temp_change != null) {
             heat_capacity = heat / (mass * temp_change);
             make_bold = 3;
-        } else if (heat && mass && heat_capacity) {
+        } else if (heat != null && mass != null && heat_capacity != null) {
             temp_change = heat / (mass * heat_capacity);
             make_bold = 4;
         }
     }
 </script>
 
+<style>
+    button {
+        border: none;
+        background-color: lightcoral;
+        padding: 8px 16px;
+        margin: 10px 0px;
+        transition: 300ms ease;
+        outline: none !important;
+    }
+
+    button:hover {
+        background-color: red;
+    }
+</style>
+
 <h3>q = mcΔT</h3>
 <table>
     <tr>
-        <th style="font-weight: {make_bold == 1 ? 'bold' : 'normal'}">ΔHeat (J)</th>
-        <th style="font-weight: {make_bold == 2 ? 'bold' : 'normal'}">Mass (g)</th>
-        <th style="font-weight: {make_bold == 3 ? 'bold' : 'normal'}">Specific Heat Capacity (J/g°C)</th>
-        <th style="font-weight: {make_bold == 4 ? 'bold' : 'normal'}">Change in temp (Final Temp (°C) - Initial Temp (°C))</th>
+        <th style="font-weight: {make_bold == 1 ? 'bold' : 'normal'}">
+            ΔHeat (J)
+        </th>
+        <th style="font-weight: {make_bold == 2 ? 'bold' : 'normal'}">
+            Mass (g)
+        </th>
+        <th style="font-weight: {make_bold == 3 ? 'bold' : 'normal'}">
+            Specific Heat Capacity (J/g°C)
+        </th>
+        <th style="font-weight: {make_bold == 4 ? 'bold' : 'normal'}">
+            Change in temp (Final Temp (°C) - Initial Temp (°C))
+        </th>
     </tr>
     <tr>
-        <td><input style="font-weight: {make_bold == 1 ? 'bold' : 'normal'}" bind:value={heat} type="number" placeholder="0" /></td>
-        <td><input style="font-weight: {make_bold == 2 ? 'bold' : 'normal'}" bind:value={mass} type="number" placeholder="0" /></td>
-        <td><input style="font-weight: {make_bold == 3 ? 'bold' : 'normal'}" bind:value={heat_capacity} type="number" placeholder="0" /></td>
-        <td><input style="font-weight: {make_bold == 4 ? 'bold' : 'normal'}" bind:value={temp_change} type="number" placeholder="0" /></td>
+        <td>
+            <input
+                style="font-weight: {make_bold == 1 ? 'bold' : 'normal'}"
+                bind:value={heat}
+                type="number"
+                placeholder="0" />
+        </td>
+        <td>
+            <input
+                style="font-weight: {make_bold == 2 ? 'bold' : 'normal'}"
+                bind:value={mass}
+                type="number"
+                placeholder="0" />
+        </td>
+        <td>
+            <input
+                style="font-weight: {make_bold == 3 ? 'bold' : 'normal'}"
+                bind:value={heat_capacity}
+                type="number"
+                placeholder="0" />
+        </td>
+        <td>
+            <input
+                style="font-weight: {make_bold == 4 ? 'bold' : 'normal'}"
+                bind:value={temp_change}
+                type="number"
+                placeholder="0" />
+        </td>
     </tr>
 </table>
-<button on:click={handleClick}>
-	Reset
-</button>
+<button on:click={handleClick}> Reset </button>
 <ul>
     <li>
         <b style="font-weight: {make_bold == 1 ? 'bold' : 'normal'}">
@@ -67,17 +127,3 @@
         </b>
     </li>
 </ul>
-<style>
-    button {
-        border: none;
-        background-color: lightcoral;
-        padding: 8px 16px;
-        margin:  10px 0px;
-        transition: 300ms ease;
-        outline: none !important;
-    }
-
-    button:hover {
-        background-color: red;
-    }
-</style> 
