017f13c52be6ec475d1a092b906cefd443cffcb1
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2021-01-12 22:27:49 -0800

    done with this section

---
+++ src/components/units/6/heat_mass_fussion/hreq_mass.svelte
@@ -0,0 +1,23 @@
+<script>
+    let hreq;
+    let heat;
+    let mass;
+
+    $: heat = hreq * mass;
+</script>
+<h3>ΔHeat from H<sub>req</sub> and Mass</h3>
+<table>
+    <tr>
+        <th>Mass (mols)</th>
+        <th>H<sub>req</sub></th>
+    </tr>
+    <tr>
+        <td><input bind:value={mass} type="number" placeholder="0"/></td>    
+        <td><input bind:value={hreq} type="number" placeholder="0"/></td>    
+    </tr>
+</table>
+<ul>
+    <li>
+        {hreq == null || isNaN(hreq)? "Hreq": hreq} (J/g) * {mass == null? "Mass (mols)": mass} = <b>{heat == null? "ΔHeat": heat}</b>
+    </li>
+</ul>
